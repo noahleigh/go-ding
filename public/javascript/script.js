2a3a578a47f7be8fc271aeeed2296beea98fad6f
noahleigh <noah.d.leigh@gmail.com>
2016-04-15 19:27:21 -0400

    Added ding when button clicked.

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,9 +1,25 @@
 var socket = io();
+var ding = new Audio('ding.mp3');
 $('#ding-button').click(function(){
+    if (!ding.paused) {
+        ding.currentTime = 0.0;
+        ding.play();
+    } else {
+        ding.play();
+    }
     socket.emit('clicked');
     return false;
 });
 
 socket.on('increment-counter', function(counter){
     $('#counter').text(counter);
+});
+
+socket.on('play-ding', function(){
+    if (!ding.paused) {
+        ding.currentTime = 0.0;
+        ding.play();
+    } else {
+        ding.play();
+    }
 });
